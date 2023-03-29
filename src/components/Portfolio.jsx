93b58d1c5945a6943cadfa0eb5bc08e23cc86e7c
mfdev <mfdevbox@gmail.com>
2023-03-30 00:21:00 +0300

    data added (portfolio)

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,22 +4,16 @@ export default function Portfolio() {
     return (
         <div className='portfolio'>
             <div>
-                <a target='_blanc' href='#'>
-                    #link
-                </a>
-                <p>#description</p>
-            </div>
-            <div>
-                <a target='_blanc' href='#'>
-                    #link
-                </a>
-                <p>#description</p>
-            </div>
-            <div>
-                <a target='_blanc' href='#'>
-                    #link
-                </a>
-                <p>#description</p>
+                <div>
+                    <a className='title' target='_blanc' href='https://mfdevutilities.vercel.app/'>
+                        Утилиты
+                    </a>
+                    <a className='github' href='https://github.com/mfdevgit/utilities' target='_blanc' />
+                </div>
+                <p>
+                    Самостоятельный проект на React (без бэка), где я собираю функционал для личного использования. Выложен на Vercel. На текущий
+                    момент есть генератор паролей и собрание градиентов.
+                </p>
             </div>
         </div>
     )
